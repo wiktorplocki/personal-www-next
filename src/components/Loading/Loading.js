031f86e63ae553f65ea5843ac73574fc769e5d9d
wiktor Płocki <wiktor.plocki@gmail.com>
2018-10-30 19:53:36 +0100

    Add spinner to Loading

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
-const Loading = () => (
-  <div className="masthead text-center d-flex">
-    <Container className="my-auto flex-center">
-      <Row>
-        <Col xl="12" lg="12" md="12" sm="12" xs="12" className="mx-auto">
-          <span>
-            <h1>
-              <strong>Loading...</strong>
-            </h1>
-          </span>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-);
+const Loading = () => {
+  /* eslint no-return-assign: "error" */
+  useEffect(() => (document.title = `Wiktor Płocki - Loading...`));
+  return (
+    <div className="masthead text-center d-flex">
+      <Container className="my-auto flex-center">
+        <Row>
+          <Col xl="12" lg="12" md="12" sm="12" xs="12" className="mx-auto">
+            <span>
+              <h1>
+                <i className="fas fa-circle-notch fa-spin" />
+                <strong> Loading...</strong>
+              </h1>
+            </span>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default Loading;
