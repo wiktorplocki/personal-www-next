c9b836efb2a81637075d96230ccf0687ce667b20
wiktor Płocki <wiktor.plocki@gmail.com>
2019-01-23 13:13:47 +0100

    Change ProjectsList, add context, styling

--- src/components/Projects/ProjectsList/ProjectsList.js
+++ src/components/Projects/ProjectsList/ProjectsList.js
@@ -12,8 +12,14 @@ import {
   Row
 } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCircleNotch,
+  faTrashAlt,
+  faEdit
+} from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+
+import AuthContext from '../../../context/AuthContext';
 
 // eslint-disable-next-line prefer-arrow-callback
 const ProjectsList = () => {
@@ -22,11 +28,8 @@ const ProjectsList = () => {
     projects {
       _id
       name
-      client
       description
-      technologies {
-        label
-      }
+      link
     }
   }`;
   // useEffect(() => async () =>
@@ -42,6 +45,7 @@ const ProjectsList = () => {
       .then(result => setProjects(result.data.projects));
   }, []);
   /* eslint no-return-assign: "error" */
+  /* eslint no-underscore-dangle: 0 */
   // eslint-disable-next-line consistent-return
   useEffect(() => {
     try {
@@ -51,42 +55,71 @@ const ProjectsList = () => {
     }
   }, []);
   return (
-    <Container className="ProjectsList">
-      <Row>
-        <Col>
-          <h1 className="mt-5 mb-5">Projects</h1>
-          <section className="mb-3">
-            {_.isEmpty(projects) ? (
-              <h2>
-                <span>
-                  <FontAwesomeIcon icon={faCircleNotch} spin />
-                </span>
-                <span> Fetching projects...</span>
-              </h2>
-            ) : (
-              <Row>
-                {projects.map(project => (
-                  <Col md="6" xs="12" key={shortid.generate()}>
-                    <Card className="project-card" key={shortid.generate()}>
-                      <CardBody>
-                        <CardTitle>{project.name}</CardTitle>
-                        <CardText>{project.description}</CardText>
-                        <CardLink tag={Link} to={`/projects/${project._id}`}>
-                          Details
-                        </CardLink>
-                        <CardLink href={`/projects/${project._id}`}>
-                          Link
-                        </CardLink>
-                      </CardBody>
-                    </Card>
-                  </Col>
-                ))}
-              </Row>
-            )}
-          </section>
-        </Col>
-      </Row>
-    </Container>
+    <AuthContext.Consumer>
+      {context => (
+        <Container className="ProjectsList">
+          <Row>
+            <Col>
+              <h1 className="mt-5 mb-5">Projects</h1>
+              <section className="mb-3">
+                {_.isEmpty(projects) ? (
+                  <h2>
+                    <span>
+                      <FontAwesomeIcon icon={faCircleNotch} spin />
+                    </span>
+                    <span> Fetching projects...</span>
+                  </h2>
+                ) : (
+                  <Row>
+                    {projects.map(project => (
+                      <Col md="6" xs="12" key={shortid.generate()}>
+                        <Card className="project-card" key={shortid.generate()}>
+                          <CardBody>
+                            <div className="d-flex flex-row justify-content-between admin-pane">
+                              <CardTitle>{project.name}</CardTitle>
+                              {context.token ? (
+                                <div className="d-flex flex-row">
+                                  <FontAwesomeIcon
+                                    icon={faEdit}
+                                    onClick={() => console.log('Edit!')}
+                                    className="mx-1 admin-icon"
+                                  />
+                                  <FontAwesomeIcon
+                                    icon={faTrashAlt}
+                                    onClick={() => console.log('Trash!')}
+                                    className="mx-1 admin-icon"
+                                  />
+                                </div>
+                              ) : null}
+                            </div>
+                            <CardText className="card-text-box">
+                              {_.truncate(project.description, {
+                                length: 203,
+                                omission: '',
+                                separator: /.? +/
+                              })}
+                            </CardText>
+                            <CardLink
+                              tag={Link}
+                              to={`/projects/${project._id}`}
+                            >
+                              Details
+                            </CardLink>
+                            {project.link && (
+                              <CardLink href={project.link}>Link</CardLink>
+                            )}
+                          </CardBody>
+                        </Card>
+                      </Col>
+                    ))}
+                  </Row>
+                )}
+              </section>
+            </Col>
+          </Row>
+        </Container>
+      )}
+    </AuthContext.Consumer>
   );
 };
 
